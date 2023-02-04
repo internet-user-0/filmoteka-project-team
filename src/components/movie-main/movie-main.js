4e00b86fef385fb8c79813f95791c99285694737
annared <annared1982@gmail.com>
2023-02-04 09:26:31 +0200

    moved fetch to API
    Привет. Перенесла функцию fetch в api.

--- src/components/movie-main/movie-main.js
+++ src/components/movie-main/movie-main.js
@@ -1,26 +1,13 @@
-const axios = require('axios').default;
+import API from '../../api';
 
-const API_KEY = '7b4917c1c89b56950d6ac1f3ef5382d2';
-const BASE_URL = 'https://api.themoviedb.org/3';
 let page = 1;
 
 showMovie();
 
 function showMovie() {
-  fetchMovie()
+  API.fetchPopularMovie(page)
     .then()
-    .catch(err => console.log(err));
+    .catch(error => console.log(error));
 }
 
-async function fetchMovie() {
-  try {
-    const response = await axios.get(
-      `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`
-    );
-    console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.log(`${error}`);
-  }
-}
-export default { showMovie, fetchMovie };
+export default { showMovie };
