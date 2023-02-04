6ee66edd3cbf636e9669106ff5bccf562691e3a3
Danil Krasnov <99325740+Hellblazer98@users.noreply.github.com>
2023-02-04 15:25:57 +0200

    add modal markup and fetch

--- src/components/film-card/fetchGenres.js
+++ src/components/film-card/fetchGenres.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'b972cd435eef10c3549386c0239d193f';
+const KEY_STORAGE_GENRES = 'genresList';
 
 const fetchGenreList = async () => {
   const response = await axios.get(
@@ -16,7 +17,7 @@ fetchGenreList()
   .then(data => {
     const genresList = data.genres;
     // console.log(genresList);
-    localStorage.setItem('genresList', JSON.stringify(genresList));
+    localStorage.setItem(KEY_STORAGE_GENRES, JSON.stringify(genresList));
   })
   .catch(err => {
     console.error(err);
@@ -25,7 +26,7 @@ fetchGenreList()
 //  Трансформация жанров в строку
 
 export function transformGenres(genreIds) {
-  const genreArr = JSON.parse(localStorage.getItem('genresList'));
+  const genreArr = JSON.parse(localStorage.getItem(KEY_STORAGE_GENRES));
   const newArr = [];
   let genresString = '';
 
@@ -41,6 +42,27 @@ export function transformGenres(genreIds) {
   }
 
   return genresString;
+}
+
+//  Трансформация жанров в строку для модалки
+
+export function transformGenresOnModal(genres) {
+    const genreArr = JSON.parse(localStorage.getItem(KEY_STORAGE_GENRES));
+    const newArr = [];
+    let genresString = '';
+
+    if (genreArr) {
+        genres.forEach(genre => {
+            genreArr.map(genreI => {
+                if (genreI.id === genre.id) {
+                    newArr.push(genreI.name);
+                    genresString = newArr[0];
+                }
+            });
+        })
+    }
+
+    return genresString
 }
 
 // console.log(transformGenres([28, 18]));
