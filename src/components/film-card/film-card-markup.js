f6cdbf9f849791ea39ba96dda7abd8a9f898eb8a
NataBelous <natabelous@icloud.com>
2023-02-08 20:03:31 +0200

    library

--- src/components/film-card/film-card-markup.js
+++ src/components/film-card/film-card-markup.js
@@ -16,6 +16,7 @@ export function createFilmCardMarkap(dataArrey) {
         id,
         release_date,
         genre_ids,
+        genres,
         poster_path,
         vote_average,
       }) => {
@@ -24,6 +25,12 @@ export function createFilmCardMarkap(dataArrey) {
           poster = `${POSTER_PATH}${poster_path}`;
         } else {
           poster = 'https://live.staticflickr.com/65535/52673964597_7ac974f3b4_k.jpg';
+        }
+        let genreNames;
+        if (genres) {
+          genreNames = genres.slice(0, 2).map(({ name }) => name).join(', ');
+        } else {
+          genreNames = transformGenres(genre_ids.slice(0, 2));
         }
         return `<li class="film-card" data-action="${id}">
         <div class="film-card__img-thumb">
@@ -36,9 +43,7 @@ export function createFilmCardMarkap(dataArrey) {
         </div>
         <h2 class="film-card__title">${title}</h2>
         <div class="film-card__thumb-info">
-        <p class="film-card__genre">${transformGenres(
-          genre_ids.slice(0, 2)
-        )} | ${release_date.slice(0, 4)}</p>
+        <p class="film-card__genre">${genreNames} | ${release_date.slice(0, 4)}</p>
           <span class="film-card__rating">${vote_average.toFixed(1)}</span>
           
         </div>
--- src/components/show-library/showLibrary.js
+++ src/components/show-library/showLibrary.js
@@ -6,17 +6,20 @@ const listLibraryEl = document.querySelector('ul[render="libraryCard"]')
 const btnQueue = document.querySelector('button[data-btn="queue"]');
 const btnWatched = document.querySelector('button[data-btn="watched"]');
 
-showStorage('watched')
-
 btnQueue.addEventListener('click', (e) => showStorage(e.target.dataset.btn));
 btnWatched.addEventListener('click', (e) => showStorage(e.target.dataset.btn));
 
 function showStorage(name) {
-   btnWatched.classList.remove("current");
-   btnQueue.classList.remove("current");
+   if (name === 'queue') {
+      btnWatched.classList.remove("current");
+      btnQueue.classList.add("current");
+   } else {
+      btnWatched.classList.add("current");
+      btnQueue.classList.remove("current");
+   }
 
    const data = Object.values(getFromStorage(name));
-
-   // e.target.classList.add("current")
-   listLibraryEl.insertAdjacentHTML('beforeend', createFilmCardMarkap(data));
+   listLibraryEl.innerHTML = createFilmCardMarkap(data);
 };
+
+showStorage('watched');
