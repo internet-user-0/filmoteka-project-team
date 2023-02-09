290c866fd71dc6c1cf5e1dfd70d38d45e52a27eb
Anton Zvertanovsky <anton.zv2002@gmail.com>
2023-02-09 18:31:11 +0200

    забирай
    )))))))))))))

--- src/components/show-library/showLibrary.js
+++ src/components/show-library/showLibrary.js
@@ -4,11 +4,17 @@ import { getFromStorage } from '../storage/storage';
 
 const listLibraryEl = document.querySelector('ul[render="libraryCard"]')
 
+
 const btnQueue = document.querySelector('button[data-btn="queue"]');
 const btnWatched = document.querySelector('button[data-btn="watched"]');
+const add = document.querySelector('.modal-wrap');
+// const btnRemove = document.querySelector('.modal-wrap');
+// console.log(btnRemove)
 
 btnQueue.addEventListener('click', (e) => showStorage(e.target.dataset.btn));
 btnWatched.addEventListener('click', (e) => showStorage(e.target.dataset.btn));
+// btnRemove.addEventListener('click', showStorage('watched'));
+
 
 function showStorage(name) {
    if (name === 'queue') {
@@ -20,7 +26,28 @@ function showStorage(name) {
    }
 
    const data = Object.values(getFromStorage(name));
-   listLibraryEl.innerHTML = createFilmCardMarkap(data);
+   if (data.length === 0) {
+      listLibraryEl.innerHTML = '<li class="defalt"></li>';
+   }else{
+      listLibraryEl.innerHTML = createFilmCardMarkap(data);
+   }
+
+
+
+   add.addEventListener('click', (e) => {
+      console.log(e.target.classList.contains(".modal__btn--remove"))
+      
+      if(e.target.classList.contains('.modal__btn--remove')){
+         console.log(e.target.classList.contains(".modal__btn--remove"))
+         const queue = JSON.parse(localStorage.getItem('queue'));
+
+         listLibraryEl.innerHTML = createFilmCardMarkap(queue)
+      }
+   });
 };
 
+
 showStorage('watched');
+
+
+
